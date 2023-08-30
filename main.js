c1328de6db68bb930309d2fdbd7d0dec7868c475
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2023-08-29 21:30:49 -0300

    add interação carrinho

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { inicializarCarrinho } from "./src/menuCarrinho";
+
 const catalogo = [{
     id: 1,
     marca: 'Zara',
@@ -65,9 +67,9 @@ const catalogo = [{
 ];
 
 for (const produtoCatalogo of catalogo) {
-    const cartaoProduto = `<div id="card-produto-1">
+    const cartaoProduto = `<div class="border-solid border-2 border-sky-500 w-48 m-2" id="card-produto-${produtoCatalogo.id}">
 <img src="assets/img/${produtoCatalogo.imagem}" alt="Produto do Magazine Bugsoft" style="height: 300px">
-<p>${produtoCatalogo.marca}</p>
+<p class="marca">${produtoCatalogo.marca}</p>
 <p>${produtoCatalogo.nome}</p>
 <p>$${produtoCatalogo.preco}</p>
 <button>Adicionar</button>
@@ -77,3 +79,4 @@ for (const produtoCatalogo of catalogo) {
 
 }
 
+inicializarCarrinho();
